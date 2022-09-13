6c66bb81ab481a55089a04776ec7ccf4cc4aee25
Musa UYUMAZ <musauyumaznotes@gmail.com>
2022-09-13 17:07:04 +0300

    13/09/2022

---
+++ Zikriye Ürkmez Cengiz/.NetCore/Pratikler/yeniklasor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace yeniklasor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
