15dce7aba6ce3eb7915c5b9ae52caf3c92da4dac
Musa UYUMAZ <113431738+musauyumaznotes@users.noreply.github.com>
2022-12-20 20:37:12 +0300

    20/12/2022

---
+++ ErtugrulYilmaz/SourceCode/Trying/Trying/Models/DropdownProps.cs
@@ -0,0 +1,9 @@
+﻿namespace Trying.Models
+{
+    public class DropdownProps
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
